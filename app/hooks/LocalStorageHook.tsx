19e7deb5f6b1e496022069e780c8167f7a354208
Hamza Alibrahim <hamza.outlookaa@gmail.com>
2024-09-18 21:28:16 +0300

    Edit to the localStorage hook

--- app/hooks/LocalStorageHook.tsx
+++ app/hooks/LocalStorageHook.tsx
@@ -4,14 +4,22 @@ import { useEffect, useState } from "react";
 
 const useLocalStorage = (key: string, defaultValue: any) => {
   const [value, setValue] = useState(() => {
+    if (typeof window === "undefined")
+      return defaultValue instanceof Function ? defaultValue() : defaultValue;
+
     const jsonValue = localStorage.getItem(key);
     if (jsonValue !== null) return JSON.parse(jsonValue);
     else if (typeof defaultValue === "function") return defaultValue();
     return defaultValue;
   });
+
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
+    if (typeof window !== "undefined") {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
   }, [key, value]);
-  return [value, setValue];
+
+  return [value, setValue] as const;
 };
+
 export default useLocalStorage;
